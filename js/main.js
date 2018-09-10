306d423c8094fa62ecdf11769e13a1c5e4f843f6
Graham Spencer <grahamspence@gmail.com>
2018-09-10 17:05:53 -0400

    exercise complete, forgot to commit as i go whoopsies

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(){
+  var switchOff = document.querySelector('.switch');
+  var shade = document.querySelector('.status');
+  var lightLevel = document.querySelector('body');
 
+  switchOff.addEventListener('click', function() {
+    switchOff.classList.toggle('off');
+    switchOff.classList.toggle('on');
+    lightLevel.classList.toggle('dark');
+    lightLevel.classList.toggle('light');
 
+    if (switchOff.classList.contains('off')) {
+      shade.innerHTML = "Who turned out the lights?";
+    } else {
+      shade.innerHTML = "It's so bright in here!";
+    }
+
+  });
+});
